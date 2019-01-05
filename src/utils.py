da686029302769b23edc61a0b580073d6e7f28fa
Pablo Macías <pamaciasm@alumnos.unex.es>
2019-01-05 12:20:11 +0100

    Some Changes

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ from os import path
 
 def get_file(url):
     """
-
+    TODO DOCUMENTATION
     :param url:
     :type url: str
     :return:
@@ -13,8 +13,3 @@ def get_file(url):
     filename = url.split('/')[-1]
     print(filename)
     open(path.join('downloads', filename), 'wb').write(response.content)
-
-
-if __name__ == '__main__':
-    get_file(
-        'https://prisa-es.mc.tritondigital.com/HOY_POR_HOY_CADENASER_222_P/media/2018/11/23/cadenaser_hoyporhoy_20181123_100000_110000.mp3')
